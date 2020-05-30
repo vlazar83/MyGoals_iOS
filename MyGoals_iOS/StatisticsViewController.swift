5dfbccfddf27de076b3134cfab8a6460fd41debc
admin <admin@admins-MacBook-Pro.local>
2020-05-30 09:03:15 +0200

    Statistics calcualtion for the other cards

--- MyGoals_iOS/StatisticsViewController.swift
+++ MyGoals_iOS/StatisticsViewController.swift
@@ -13,12 +13,19 @@ class StatisticsViewController: UIViewController {
 
     @IBOutlet weak var redCardsStatisticsLabel: UILabel!
     
+    @IBOutlet weak var blueCardsStatisticsLabel: UILabel!
+    
+    @IBOutlet weak var greenCardsStatisticsLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
         redCardsStatisticsLabel.text = String(Statistics.shared.getStatisticsForDay(day: Utils.getDayOfYear()).redCardCount)
+        
+        blueCardsStatisticsLabel.text = String(Statistics.shared.getStatisticsForDay(day: Utils.getDayOfYear()).blueCardCount)
+        
+        greenCardsStatisticsLabel.text = String(Statistics.shared.getStatisticsForDay(day: Utils.getDayOfYear()).greenCardCount + Statistics.shared.getStatisticsForDay(day: Utils.getDayOfYear()).lightGreenCardCount)
         
     }
     
