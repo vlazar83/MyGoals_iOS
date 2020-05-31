0fb458743a2b05347cba829d49e2477251405178
admin <admin@admins-MacBook-Pro.local>
2020-05-31 14:40:45 +0200

    minor fix for statusBar display

--- MyGoals_iOS/ViewControllerHomePage.swift
+++ MyGoals_iOS/ViewControllerHomePage.swift
@@ -46,6 +46,22 @@ class ViewControllerHomePage: UIViewController, RefreshCardsDelegateProtocol {
         
     }
     
+    func setStatusBar(backgroundColor: UIColor) {
+        let statusBarFrame: CGRect
+        if #available(iOS 13.0, *) {
+            statusBarFrame = view.window?.windowScene?.statusBarManager?.statusBarFrame ?? CGRect.zero
+        } else {
+            statusBarFrame = UIApplication.shared.statusBarFrame
+        }
+        let statusBarView = UIView(frame: statusBarFrame)
+        statusBarView.backgroundColor = backgroundColor
+        view.addSubview(statusBarView)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        setStatusBar(backgroundColor: UIColor.black)
+    }
+    
     private func setupStatistics(){
         
         let yearOfSavedStatistics = Utils.loadStatisticsYearFromUserDefaults()
